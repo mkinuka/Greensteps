01412bebbaac645982dda1f6d07a4dc39e076167
Moses Kinuka <kinuka__@hotmail.com>
2025-12-04 15:44:06 +0100

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { router } from './Router'
 
 function App() {
   return (
     <>
+    <RouterProvider router={router}></RouterProvider>
 
     </>
   )
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,7 +1,28 @@
-export const Navigation = () => {
+import { NavLink } from "react-router-dom"
 
+export const Navigation = () => {
+const links = [
+  { to: "/dashboard", label: "Dashboard" },
+  { to: "/food", label: "Food" },
+  { to: "/transport", label: "Transport" },
+  { to: "/shopping", label: "Shopping" },
+  { to: "/electricity", label: "Electricity" },
+  { to: "/about", label: "About" }
+];
     return <>
-    
+    <nav>
+    {links.map(link => (
+        <li key={link.to}>
+            <NavLink to={link.to} className={({ isActive }) =>
+            isActive
+          ? "text-blue-600 font-semibold underline" 
+          : "text-gray-800 hover:text-blue-600"        
+        }
+                >{link.label}</NavLink>
+        </li>
+    ))}
+  
+    </nav>
     
     </>
 }
